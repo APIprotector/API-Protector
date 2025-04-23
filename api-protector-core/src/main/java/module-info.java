54fdde384de36fa3fcca3d122122b73dcf5118be
Arkadiusz Błasiak <arkbl13@gmail.com>
2025-04-24 00:31:25 +0200

    changes structure

--- api-protector-core/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module api.protector.core {
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires io.swagger.v3.oas.models;
-    requires openapi.diff.core;
-}
