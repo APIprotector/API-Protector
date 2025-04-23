d26bef4d97c72316d6cc8130dec9e85f8d86085b
Arkadiusz Błasiak <arkbl13@gmail.com>
2025-04-23 22:48:27 +0200

    init

---
+++ api-protector-core/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module api.protector.core {
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires io.swagger.v3.oas.models;
+    requires openapi.diff.core;
+}
