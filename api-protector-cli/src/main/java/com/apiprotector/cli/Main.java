e2e7e393ce3f049f49dbd4c8e14f935a1d4ffb66
Arkadiusz Błasiak <arkbl13@gmail.com>
2025-05-13 22:23:22 +0200

    bugfixes, new features, refactor. IT IS 2.1.0-SNAPSHOT VERSION!! 🎉

--- api-protector-cli/src/main/java/com/apiprotector/cli/Main.java
+++ api-protector-cli/src/main/java/com/apiprotector/cli/Main.java
@@ -15,17 +15,10 @@ import java.nio.file.Path;
 
 
 public class Main {
-    private static final String USAGE = "API Protector <OLD_FILE> <NEW_FILE> [-o outputfile]";
+    private static final String USAGE = "<OLD_FILE> <NEW_FILE> [OPTIONS]";
     public static void main(String[] args) {
 
-        Options options = new Options();
-        Option output = Option.builder("o")
-                .longOpt("output")
-                .hasArg()
-                .desc("Output file")
-                .build();
-
-        options.addOption(output);
+        Options options = setOptions();
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -33,50 +26,39 @@ public class Main {
         try {
             CommandLine cmd = parser.parse(options, args);
 
+            if (cmd.hasOption("h")) {
+                formatter.printHelp(USAGE, options);
+                System.exit(0);
+            }
+
             String[] remainingArgs = cmd.getArgs();
             if (remainingArgs.length < 2) {
-                System.out.println("Error: Missing required arguments <OLD_FILE> <NEW_FILE>");
+                System.err.println("Error: missing required arguments <OLD_FILE> <NEW_FILE>");
+                formatter.printHelp(USAGE, options);
+                System.exit(1);
+            }
+            else if (remainingArgs.length > 2) {
+                System.err.println("Error: too many arguments");
                 formatter.printHelp(USAGE, options);
                 System.exit(2);
             }
 
             String previousFile = remainingArgs[0];
             String currentFile = remainingArgs[1];
-            String previousFileContent = null;
-            String currentFileContent = null;
+            String previousFileContent;
+            String currentFileContent;
             String result = null;
 
             ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
 
             try {
-                if (previousFile.endsWith("yaml")) {
-                    previousFileContent = yamlMapper.readTree(new File(previousFile)).toString();
-                }
-                else if (previousFile.endsWith("json")) {
-                    previousFileContent = Files.readString(Path.of(previousFile));
-                }
-                else {
-                    System.err.println("Unsupported file format. Only YAML and JSON are supported.");
-                    System.exit(3);
-                }
-
-                if (currentFile.endsWith("yaml")) {
-                    currentFileContent = yamlMapper.readTree(new File(currentFile)).toString();
-                }
-                else if (currentFile.endsWith("json")) {
-                    currentFileContent = Files.readString(Path.of(currentFile));
-                }
-                else {
-                    System.err.println("Unsupported file format. Only YAML and JSON are supported.");
-                    System.exit(3);
-                }
+                previousFileContent = getFileContent(previousFile, yamlMapper);
+                currentFileContent = getFileContent(currentFile, yamlMapper);
                 result = new CompositeDiff(previousFileContent, currentFileContent).toString();
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 System.err.println("Error while reading file: " + e.getMessage());
                 System.exit(4);
-            } catch (Exception e) {
-                System.err.println("Error while processing files:; " + e.getMessage());
-                System.exit(5);
             }
 
             ObjectMapper jsonMapper = new ObjectMapper();
@@ -88,29 +70,88 @@ public class Main {
             if (cmd.hasOption("o")) {
                 String outputFile = cmd.getOptionValue("o");
                 try (FileWriter writer = new FileWriter(outputFile)) {
-                    writer.write(changes);
-                    writer.write(compatibilityInfo);
-                    System.out.println("Output written to " + outputFile);
-                } catch (IOException e) {
-                    System.err.println("Error writing to file: " + e.getMessage());
-                    System.exit(6);
+                    if (cmd.hasOption("v")) {
+                        writer.write(changes);
+                    }
+                    else {
+                        writer.write(compatibilityInfo);
+                    }
+                    if (!cmd.hasOption("s")) {
+                        System.out.println("Output written to " + outputFile);
+                    }
                 }
-            } else {
-                System.out.println(changes);
-                System.out.println(compatibilityInfo);
+                catch (IOException e) {
+                    System.err.println("Error writing to file: " + e.getMessage());
+                    System.exit(5);
+                }
+            }
+            else {
+                if (cmd.hasOption("v")) {
+                    System.out.println(changes);
+                }
+                else if (!cmd.hasOption("s")) {
+                    System.out.println(compatibilityInfo);
+                }
             }
 
-            if (!isCompatible) {
+            if (!isCompatible && cmd.hasOption("e")) {
                 System.exit(1);
             }
-        } catch (ParseException e) {
-            System.err.println("Parsing failed: " + e.getMessage());
+
+        }
+        catch (AlreadySelectedException e) {
+            System.err.println("Multiple options selected: " + e.getMessage());
             formatter.printHelp(USAGE, options);
             System.exit(6);
-        } catch (JsonProcessingException e) {
-            System.err.println("Error processing JSON: " + e.getMessage());
+        }
+        catch (ParseException e) {
+            System.err.println("Parsing failed: " + e.getMessage());
+            formatter.printHelp(USAGE, options);
             System.exit(7);
         }
+        catch (JsonProcessingException e) {
+            System.err.println("Error processing JSON: " + e.getMessage());
+            System.exit(8);
+        }
+        catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+            System.exit(9);
+        }
+    }
 
+    private static Options setOptions() {
+        Options options = new Options();
+        options.addOption("h", "help"
+                , false, "Display help information");
+        options.addOption("o", "output"
+                , true, "Output file");
+        options.addOption("e", "error"
+                , false, "Exit with error code if the OpenAPI changes are not compatible");
+
+        OptionGroup optionGroup = new OptionGroup();
+        Option verboseOption = new Option("v", "verbose"
+                , false, "Verbose output");
+        Option silentOption = new Option("s", "silent"
+                , false, "Print no output to console");
+
+        optionGroup.addOption(verboseOption);
+        optionGroup.addOption(silentOption);
+        options.addOptionGroup(optionGroup);
+        return options;
+    }
+
+    private static String getFileContent(String fileName, ObjectMapper yamlMapper) throws IOException {
+        String previousFileContent = null;
+        if (fileName.endsWith(".yaml") || fileName.endsWith(".yml")) {
+            previousFileContent = yamlMapper.readTree(new File(fileName)).toString();
+        }
+        else if (fileName.endsWith(".json")) {
+            previousFileContent = Files.readString(Path.of(fileName));
+        }
+        else {
+            System.err.println("Unsupported file format. Only YAML and JSON are supported.");
+            System.exit(3);
+        }
+        return previousFileContent;
     }
 }
